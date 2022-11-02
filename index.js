e0ac896ab1dc2fed6e62ac1a5db08552a29ab8a9
Chase Maxfield <maxfield.chase@gmail.com>
2022-11-02 13:25:00 -0600

    two new pages

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
+app.get('/data', function (req, res) {
+    res.render('pages/data');
+});
+
+app.get('/reports', function (req, res) {
+    res.render('pages/reports');
+});
+
 app.listen(80);
 console.log("Server listening on port 80.");
