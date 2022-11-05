6a90b8bee033cb72cd47976c071930f7b4c56403
Chase Maxfield <maxfield.chase@gmail.com>
2022-11-05 11:10:03 -0600

    Fixed links

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
+app.get(['/', '/index'], function (req, res) {
     res.render('pages/index');
 });
 
